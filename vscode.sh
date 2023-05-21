0940c10db19e8e432568ea218bf8b83571b3fc3c
Aaditya Thakur <athakur4_be22@thapar.edu>
2023-05-21 19:12:48 +0530

    hacky but works :)

--- vscode.sh
+++ vscode.sh
@@ -1,5 +1,13 @@
 mkdir temp
 cd temp
-# wget https://code.visualstudio.com/sha/download?build=stable&os=linux-x64 -v
-tar -xf code* && cd VSCode*
-
+echo "Downloading VSCode..."
+wget -nv https://code.visualstudio.com/sha/download?build=stable\&os=linux-x64
+mv * code.tar.gz
+echo "Installing..."
+tar -xf code*
+sudo mv VS* /usr/share/VSCode
+sudo ln -s /usr/share/VSCode/code /usr/local/bin/
+wget -nv https://github.com/aadi-1024/vscode-arch/blob/master/vscode.png?raw=true
+sudo mv vscode.png?raw=true /usr/share/pixmaps/code.png
+wget -nv https://raw.githubusercontent.com/aadi-1024/vscode-arch/master/code.desktop
+sudo mv code.desktop /usr/share/applications
